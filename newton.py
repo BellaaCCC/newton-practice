349a177c7b79dcfb0de2a8fb2b58475d26de93b9
Bella <bella.jh.chen@berkeley.edu>
2026-08-19 14:27:15 -0700

    Update newton.py

--- newton.py
+++ newton.py
@@ -1,16 +1,19 @@
-#Newton's method
-
-    
+#Newton's method 
 def first_derivative(f, x, h=0.001):
+    """#calculates the value of the first derivative of the function f, at the value x"""
     x_first = (f(x+h) - f(x))/ h
     return x_first
 
+
 def second_derivative(f,x, h=0.001):
+    """calculates the value of the second derivative of the function f, at the value x"""
     x_second = (f(x+h) - 2*f(x) + f(x-h)) / h**2
     # or： x_second = (first_derivative(f,x+h)- first_derivative(f,x)) / h
     return x_second
-    
-def Newtons(f, x, diff = 0.001, max_iteration = 1000):
+
+
+def optimize(f, x, diff = 0.001, max_iteration = 1000):
+    """Optimaises the function f, starting from value x"""
     iteration = 1
     while iteration < max_iteration:
         x_first = first_derivative(f, x)
